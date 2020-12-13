067736a7339a28026c988ec94acc6042b1ab4b3c
debilla <dwckrm@gmail.com>
2020-12-14 00:08:30 +0530

    type issue fixed

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const ProductSchema = mongoose.Schema({
     title: String,
     description: String,
-    ratingValue: Number,
-    reviewCount: Number,
+    ratingValue: String,
+    reviewCount: String,
     venderName: String,
-    price: Number,
+    price: String,
     addedDate: {
         type: Date,
         default: Date.now
